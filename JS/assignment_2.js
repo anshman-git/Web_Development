38474de437e033df43964c4ebce9836ba980f256
Anshman Khare <anshmankhare@gmail.com>
2026-07-14 23:59:02 +0530

    javascript_assignments_3

--- JS/assignment_2.js
+++ JS/assignment_2.js
@@ -16,41 +16,41 @@ console.log(res);
 //solution
 // let arr = [1,2,3,4,5];
 // let n = 3;
-let res = slice(arr.length-n)
+// let res = slice(arr.length-n)
 
 // Qs3. Write a JavaScript program to check whether a string is blank or not.
 //solution 
-let str = prompt("Please enter a string");
-if(str.length == 0) {
-    console.log("string is empty"); 
-} else {
-    console.log("string is not empty");
-}
+// let str = prompt("Please enter a string");
+// if(str.length == 0) {
+//     console.log("string is empty"); 
+// } else {
+//     console.log("string is not empty");
+// }
 
 // Qs4. Write a JavaScript program to test whether the character at the given (character)
 // index is lower case.
 
 //solution 
-let str="AnsHMan";
-let ind = 2;
-if(str[ind] == str[ind].toLowerCase()) {
-    console.log("character is lowercase");
-} else {
-    console.log("character is not lowercase");
-}
+// let str="AnsHMan";
+// let ind = 2;
+// if(str[ind] == str[ind].toLowerCase()) {
+//     console.log("character is lowercase");
+// } else {
+//     console.log("character is not lowercase");
+// }
 
 // Qs5. Write a JavaScript program to strip leading and trailing spaces from a string.
 
 //solution
-let str = prompt("Please enter a string");
-console.log('original string = ${str}');
-console.log('trailing space removed = ${str.trim()}');
+// let str = prompt("Please enter a string");
+// console.log('original string = ${str}');
+// console.log('trailing space removed = ${str.trim()}');
 
 // Qs6. Write a JavaScript program to check if an element exists in an array or not.
-let arr=["hello",'a',23,64,99,-6];
-let item=64;
-if(arr.indexOf(item)!=-1){
-    console.log("element exists in array");
-}else{
-    console.log("element doesn't exist in array");
-}
+// let arr=["hello",'a',23,64,99,-6];
+// let item=64;
+// if(arr.indexOf(item)!=-1){
+//     console.log("element exists in array");
+// }else{
+//     console.log("element doesn't exist in array");
+// }
