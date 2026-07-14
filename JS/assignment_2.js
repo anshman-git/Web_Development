4932f6262ac5a10eaab13af7dcd9691735786f24
Anshman Khare <anshmankhare@gmail.com>
2026-07-14 23:57:40 +0530

    javascript_assignments

---
+++ JS/assignment_2.js
@@ -0,0 +1,56 @@
+// Qs1. Write a JavaScript program to get the first n elements of an array. [n can be any
+// positive number].
+// For example: for array [7, 9, 0, -2] and n=3
+// Print, [7, 9, 0]
+
+//solution
+let arr = [1,2,3,4,5];
+let n = 3;
+let res = arr.slice(0,n);
+console.log(res);
+
+
+// Qs2. Write a JavaScript program to get the last n elements of an array. [n can be any
+// positive number].
+
+//solution
+// let arr = [1,2,3,4,5];
+// let n = 3;
+let res = slice(arr.length-n)
+
+// Qs3. Write a JavaScript program to check whether a string is blank or not.
+//solution 
+let str = prompt("Please enter a string");
+if(str.length == 0) {
+    console.log("string is empty"); 
+} else {
+    console.log("string is not empty");
+}
+
+// Qs4. Write a JavaScript program to test whether the character at the given (character)
+// index is lower case.
+
+//solution 
+let str="AnsHMan";
+let ind = 2;
+if(str[ind] == str[ind].toLowerCase()) {
+    console.log("character is lowercase");
+} else {
+    console.log("character is not lowercase");
+}
+
+// Qs5. Write a JavaScript program to strip leading and trailing spaces from a string.
+
+//solution
+let str = prompt("Please enter a string");
+console.log('original string = ${str}');
+console.log('trailing space removed = ${str.trim()}');
+
+// Qs6. Write a JavaScript program to check if an element exists in an array or not.
+let arr=["hello",'a',23,64,99,-6];
+let item=64;
+if(arr.indexOf(item)!=-1){
+    console.log("element exists in array");
+}else{
+    console.log("element doesn't exist in array");
+}
