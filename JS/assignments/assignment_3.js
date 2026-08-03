5947808ee59fc1ce9ad8f647adc118ee1840d416
Anshman Khare <anshmankhare@gmail.com>
2026-08-03 23:53:09 +0530

    js assignments

---
+++ JS/assignments/assignment_3.js
@@ -0,0 +1,40 @@
+// 1. Write a JavaScript program to get the first n elements of an array .[n can be any positive number]
+let arr = [1,2,3,4,5];
+let n = 2;
+let ans = arr.slice(0,n);
+
+// 2.Write a JavaScript program to get the last n elements of an array.
+let arr = [1,2,3,4,5];
+let n = 2;
+let ans = arr.slice(arr.length-n)
+
+// 3.Write a JavaScript program to check whether a string is blank or not.
+let str = prompt("Enter string:");
+if(str.length == 0) {
+    console.log("empty string");
+} else {
+    console.log("not empty");
+}
+// 4.Write a JavaScript program to test whether the character at the given(character)index is lowercase.
+let str = "AnsHMan";
+let index = 2;
+
+if(str[index] == str[index].toLocaleLowerCase()) {
+    console.log("lowercase");
+} else {
+    console.log("Uppercase");
+}
+// 5.Write a JavaScript program to stripleading and trailing  spaces from a string. 
+let str = prompt("Enter your string");
+console.log("original string = ${str}");
+console.log("new string = ${str.trim()}");
+
+// 6.Write a JavaScript program to check if a n element exists in an array or not.
+let arr = ["ansh",'a',22,43,234];
+let item = 32;
+if(arr.indexOf(item) != -1) {
+    console.log("element exists in array");
+}
+else {
+    console.log("element doesn't exists in array");
+}
