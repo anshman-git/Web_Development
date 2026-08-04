ceb74dc0637d4296b7438d85f25694181f509d11
Anshman Khare <anshmankhare@gmail.com>
2026-08-04 23:55:43 +0530

    js assignments 4

---
+++ JS/assignments/assignment_4.js
@@ -0,0 +1,5 @@
+// 1.Write a Js program to delete all occurances of element num in a given Array.
+// 2. write a js program to find the no of digit in a Number
+// 3. Write a js program to find the sum of digit in a Number.
+// 4. Print the factorial of a number.
+// 5. Find the largest number in an array with only positive number.
