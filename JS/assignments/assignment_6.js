8e3601b248d892e6030e80610a6c596b9f6121b9
Anshman Khare <anshmankhare@gmail.com>
2026-08-08 23:51:49 +0530

    js assignments 6

--- JS/assignments/assignment_6.js
+++ JS/assignments/assignment_6.js
@@ -1,1 +1,9 @@
+// 1. Write a js fucntion that returns array elements larger than the number.
 
+// 2. Write js function to extract unique chatacters from a string.
+
+// 3. Write a js function that accepts a list of country names as input and returns the longest country name as output.
+
+// 4. Write a js function to count the number of vovels in a string arguments.
+
+// 5. Write a js function to generate a random number within a range (start, end).
